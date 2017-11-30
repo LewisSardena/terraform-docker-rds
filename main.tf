46a6470ae8697f69a665a3461f30346801d48848
LewisSardena <lewis.sardena@me.com>
2017-11-30 12:35:15 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,8 +7,8 @@ variable "aws_secret_key" {}
 variable "private_key_path" {}
 variable "key_name" {}
 variable "region" {}
-variable "db_username {}
-variable "db_password {}
+variable "db_username" {}
+variable "db_password" {}
 ##################################################################################
 # PROVIDERS
 ##################################################################################
